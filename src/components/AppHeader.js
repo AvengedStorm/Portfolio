4938e5d0eb0017075a9f5b345a76a17f7b87d981
omri <DJCreeperNo1@gmail.com>
2021-05-14 12:40:33 +0300

    changing relative paths

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -14,9 +14,9 @@ const header = () => {
         <div style={{textAlign: 'left', marginLeft: "2.5%",paddingTop: "1.25%"}}>
             <a id="button" href="/"><h1>My Portfolio Site</h1></a>
             <ul style={{listStyleType: "none", width: "5%"}}>
-                <a href="/projects"><li id="button">My Projects</li></a>
-                <a href="/cv"><li id="button">My C.V.</li></a>
-                <a href="/about"><li id="button">About Me</li></a>
+                <a href="projects"><li id="button">My Projects</li></a>
+                <a href="cv"><li id="button">My C.V.</li></a>
+                <a href="about"><li id="button">About Me</li></a>
             </ul>
         </div>
     )
